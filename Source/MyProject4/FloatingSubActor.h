7691f5127b9da40239aa54ecfb517f4f9d234846
ColeDNate <colednate@gmail.com>
2018-04-19 15:51:40 -0700

    Add files via upload

---
+++ Source/MyProject4/FloatingSubActor.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FloatingActor.h"
+#include "FloatingSubActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT4_API AFloatingSubActor : public AFloatingActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
